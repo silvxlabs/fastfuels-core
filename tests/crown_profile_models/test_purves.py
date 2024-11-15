61ae3bbb3114d17f8e5835f20880bc69bf679d33
armjay <45674942+jacobarmbrecht@users.noreply.github.com>
2024-11-15 12:38:35 -0700

    Added functionality to create tree list of all trees in SPCD list. Created iterative test to test all trees in list for PurvesCrownProfile. Added more test cases along length of maximum tree height. Added comments.

--- tests/crown_profile_models/test_purves.py
+++ tests/crown_profile_models/test_purves.py
@@ -11,6 +11,7 @@ import pytest
 import numpy as np
 
 #generates a list of all the trees in the species code index
+#the returned list is formatted in the particular way of PurvesCrownProfile models
 @pytest.fixture()
 def purves_tree_list():
     trees = make_tree_list("purves")
@@ -25,7 +26,7 @@ def purves_tree_list():
     return tree_list
 
 
-
+#a test sample model we could use
 @pytest.fixture
 def model():
     # Suppose that I have a tree with a height of 10m, dbh of 15cm, and crown ratio of 0.4
@@ -43,14 +44,16 @@ def custom_model(scope="module"):
         )
     return _model
 
-
-@pytest.mark.parametrize("test_input", [0,61,-1,5, 10, -1, 11])
+#parameters that test edge cases as well as cases within tree
+@pytest.mark.parametrize("test_input", [0,61,-1,1,5,10,11,12,13,14,15,16,17,18,19,20,25,30,35,40,45,50,55,60])
 class TestPurvesCrownProfile:
 
+    #tests all trees in the species code list
     def test_all_trees(self, purves_tree_list, test_input):
         for tree in purves_tree_list:
             self.test_tree(tree, test_input)
 
+    #tests individual trees and determines which test to run based on tree height and crown ratio
     def test_tree(self, tree, test_input):
         if test_input > tree.height:                        #above tree
             self.test_grah_rad_0(tree, test_input)
