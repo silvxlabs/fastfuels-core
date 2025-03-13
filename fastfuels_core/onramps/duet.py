3f47cde96e4f01d96fa5541a8c5ed72b510b8533
ntutland <56928897+ntutland@users.noreply.github.com>
2025-03-13 11:32:00 -0400

    Trim to only include run_duet()

--- fastfuels_core/onramps/duet.py
+++ fastfuels_core/onramps/duet.py
@@ -14,17 +14,15 @@ from subprocess import run, PIPE
 from time import sleep
 
 # External imports
-import duet_tools as duet
-from duet_tools.inputs import InputFile
+from duet_tools import DuetRun, InputFile, import_duet
 from duet_tools.utils import write_array_to_dat
 import numpy as np
-from shapely import Polygon
-from xarray import DataArray, Dataset
+from xarray import DataArray
 
 DATA_PATH = files("fastfuels_core.data")
 
 
-def run_duet_no_calibration(
+def run_duet(
     duet_exe_directory: Path | str,
     duet_exe_name: str,
     canopy_grid: DataArray,
@@ -34,268 +32,7 @@ def run_duet_no_calibration(
     duration: int,
     random_seed: int = None,
     duet_version: str = "v2",
-) -> Dataset:
-    """
-    Runs the specified DUET executable using the supplied treeGrid.
-    No calibration of values is performed.
-
-    Parameters
-    ----------
-    duet_exe_directory: Path | str
-        Path to directory containing the DUET executable.
-    duet_exe_name: str
-        Name of DUET exe file, with extension.
-    bulk_density_grid: DataArray
-        Grid of bulk density values with dimensions ["z", "y", "x"].
-    species_code_grid: DataArray
-        Grid of FIA species codes with dimensions ["z", "y", "x"].
-    fuel_moisture_grid: DataArray
-        Grid of fuel moisture content values with dimensions ["z", "y", "x"].
-    wind_direction: float
-        Direction of prevailing wind in DUET simulation (degrees)
-    wind_variability: float
-        Variability in wind direction for the DUET simulation (degrees)
-    random_seed: int = None
-        Seed for reproducibility in DUET. If None, will be generated from computer time.
-    duet_version: str = "v2"
-        Version of DUET executable. Accepts "v1" and "v2".
-
-    Returns
-    -------
-    An Xarray Dataset with dimensions ["parameter","fuel_type","x","y"] shape (2,2,ny,nx).
-    """
-    duet_run = _run_duet(
-        duet_exe_directory,
-        duet_exe_name,
-        canopy_grid,
-        spcd_grid,
-        wind_direction,
-        wind_variability,
-        duration,
-        random_seed,
-        duet_version,
-    )
-    return _assemble_dataset(duet_run, canopy_grid)
-
-
-def run_and_calibrate_duet(
-    duet_exe_directory: Path | str,
-    duet_exe_name: str,
-    canopy_grid: DataArray,
-    spcd_grid: DataArray,
-    wind_direction: float,
-    wind_variability: float,
-    duration: int,
-    random_seed: int = None,
-    duet_version: str = "v2",
-    bulk_density_targets: duet.FuelParameter = None,
-    fuel_height_targets: duet.FuelParameter = None,
-) -> Dataset:
-    """
-    Runs the specified DUET executable using the supplied treeGrid, then
-    calibrates the resulting values based on targets and methods the supplied
-    FuelParameter object(s).
-
-    Parameters
-    ----------
-    duet_exe_directory: Path | str
-        Path to directory containing the DUET executable.
-    duet_exe_name: str
-        Name of DUET exe file, with extension.
-    bulk_density_grid: DataArray
-        Grid of bulk density values with dimensions ["z", "y", "x"].
-    species_code_grid: DataArray
-        Grid of FIA species codes with dimensions ["z", "y", "x"].
-    fuel_moisture_grid: DataArray
-        Grid of fuel moisture content values with dimensions ["z", "y", "x"].
-    wind_direction: float
-        Direction of prevailing wind in DUET simulation (degrees)
-    wind_variability: float
-        Variability in wind direction for the DUET simulation (degrees)
-    random_seed: int = None
-        Seed for reproducibility in DUET. If None, will be generated from computer time.
-    duet_version: str = "v2"
-        Version of DUET executable. Accepts "v1" and "v2".
-
-    Returns
-    -------
-    An Xarray Dataset with dimensions ["parameter","fuel_type","x","y"] shape (2,2,ny,nx).
-    """
-    duet_run = _run_duet(
-        duet_exe_directory,
-        duet_exe_name,
-        canopy_grid,
-        spcd_grid,
-        wind_direction,
-        wind_variability,
-        duration,
-        random_seed,
-        duet_version,
-    )
-    targets = [bulk_density_targets, fuel_height_targets]
-    fuel_parameter_targets = [target for target in targets if target is not None]
-    calibrated_duet = duet.calibrate(duet_run, fuel_parameter_targets)
-    return _assemble_dataset(calibrated_duet, canopy_grid)
-
-
-def run_and_calibrate_duet_from_landfire(
-    duet_exe_directory: Path | str,
-    duet_exe_name: str,
-    canopy_grid: DataArray,
-    spcd_grid: DataArray,
-    wind_direction: float,
-    wind_variability: float,
-    duration: int,
-    domain: Polygon,
-    epsg: int,
-    random_seed: int = None,
-    duet_version: str = "v2",
-):
-    """"""
-    duet_run = _run_duet(
-        duet_exe_directory,
-        duet_exe_name,
-        canopy_grid,
-        spcd_grid,
-        wind_direction,
-        wind_variability,
-        duration,
-        random_seed,
-        duet_version,
-    )
-    query = duet.query_landfire(domain, duet_exe_directory, epsg)
-    grass_density = duet.assign_targets_from_sb40(query, "grass", "density")
-    grass_height = duet.assign_targets_from_sb40(query, "grass", "height")
-    litter_density = duet.assign_targets_from_sb40(query, "litter", "density")
-    litter_height = duet.assign_targets_from_sb40(query, "litter", "height")
-    density_targets = duet.set_density(grass=grass_density, litter=litter_density)
-    height_targets = duet.set_height(grass=grass_height, litter=litter_height)
-    fuel_parameter_targets = [density_targets, height_targets]
-    calibrated_duet = duet.calibrate(duet_run, fuel_parameter_targets)
-    return _assemble_dataset(calibrated_duet, canopy_grid)
-
-
-def run_and_calibrate_duet_from_surface_grid(
-    duet_exe_directory: Path | str,
-    duet_exe_name: str,
-    canopy_grid: DataArray,
-    spcd_grid: DataArray,
-    wind_direction: float,
-    wind_variability: float,
-    duration: int,
-    surface_grid: Dataset,  # TODO
-    calibration_method: str,
-    random_seed: int = None,
-    duet_version: str = "v2",
-) -> Dataset:
-    """
-    Runs the specified DUET executable using the supplied treeGrid, then
-    calibrates the resulting values based on targets and methods the supplied
-    surfaceGrid.
-
-    Parameters
-    ----------
-    duet_exe_directory: Path | str
-        Path to directory containing the DUET executable.
-    duet_exe_name: str
-        Name of DUET exe file, with extension.
-    bulk_density_grid: DataArray
-        Grid of bulk density values with dimensions ["z", "y", "x"].
-    species_code_grid: DataArray
-        Grid of FIA species codes with dimensions ["z", "y", "x"].
-    fuel_moisture_grid: DataArray
-        Grid of fuel moisture content values with dimensions ["z", "y", "x"].
-    wind_direction: float
-        Direction of prevailing wind in DUET simulation (degrees)
-    wind_variability: float
-        Variability in wind direction for the DUET simulation (degrees)
-    surface_grid: Dataset
-        An Xarray Dataset with dimensios ["parameter","fuel_type","x","y"]
-    calibration_method: str
-        Method by which to summarize surface grid and calibrate DUET.
-        Must be one of ["maxmin", "meansd"].
-    random_seed: int = None
-        Seed for reproducibility in DUET. If None, will be generated from computer time.
-    duet_version: str = "v2"
-        Version of DUET executable. Accepts "v1" and "v2".
-
-    Returns
-    -------
-    An Xarray Dataset with dimensions ["parameter","fuel_type","x","y"] shape (2,2,ny,nx).
-    """
-    duet_run = _run_duet(
-        duet_exe_directory,
-        duet_exe_name,
-        canopy_grid,
-        spcd_grid,
-        wind_direction,
-        wind_variability,
-        duration,
-        random_seed,
-        duet_version,
-    )
-    grass_loading_grid = surface_grid.sel(
-        parameter="fuel_loading", fuel_type="grass"
-    ).data
-    grass_height_grid = surface_grid.sel(parameter="fuel_depth", fuel_type="grass").data
-    litter_loading_grid = surface_grid.sel(
-        parameter="fuel_loading", fuel_type="litter"
-    ).data
-    litter_height_grid = surface_grid.sel(
-        parameter="fuel_depth", fuel_type="litter"
-    ).data
-
-    if calibration_method == "maxmin":
-        grass_density = duet.assign_targets(
-            "maxmin", max=np.max(grass_loading_grid), min=np.min(grass_loading_grid)
-        )
-        grass_height = duet.assign_targets(
-            "maxmin", max=np.max(grass_height_grid), min=np.min(grass_height_grid)
-        )
-        litter_density = duet.assign_targets(
-            "maxmin", max=np.max(litter_loading_grid), min=np.min(litter_loading_grid)
-        )
-        litter_height = duet.assign_targets(
-            "maxmin", max=np.max(litter_height_grid), min=np.min(litter_height_grid)
-        )
-    if calibration_method == "meansd":
-        grass_density = duet.assign_targets(
-            "meansd", max=np.max(grass_loading_grid), min=np.min(grass_loading_grid)
-        )
-        grass_height = duet.assign_targets(
-            "meansd", max=np.max(grass_height_grid), min=np.min(grass_height_grid)
-        )
-        litter_density = duet.assign_targets(
-            "meansd", mean=np.mean(litter_loading_grid), sd=np.std(litter_loading_grid)
-        )
-        litter_height = duet.assign_targets(
-            "meansd", mean=np.mean(litter_height_grid), sd=np.std(litter_height_grid)
-        )
-
-    density_targets = duet.set_density(grass=grass_density, litter=litter_density)
-    height_targets = duet.set_height(grass=grass_height, litter=litter_height)
-    fuel_parameter_targets = [density_targets, height_targets]
-    calibrated_duet = duet.calibrate(duet_run, fuel_parameter_targets)
-    return _assemble_dataset(calibrated_duet, canopy_grid)
-
-
-# def run_and_calibrate_duet_with_mask(grass_mask, litter_mask) -> ndarray:
-#     output = run_duet(...)
-#     calibrated_output = dt.calibrate(output)
-#     return calibrated_output
-
-
-def _run_duet(
-    duet_exe_directory: Path | str,
-    duet_exe_name: str,
-    canopy_grid: DataArray,
-    spcd_grid: DataArray,
-    wind_direction: float,
-    wind_variability: float,
-    duration: int,
-    random_seed: int = None,
-    duet_version: str = "v2",
-) -> duet.DuetRun:
+) -> DuetRun:
     """
     Runs the specified DUET executable using the supplied treeGrid.
 
@@ -319,6 +56,10 @@ def _run_duet(
         Seed for reproducibility in DUET. If None, will be generated from computer time.
     duet_version: str = "v2"
         Version of DUET executable. Accepts "v1" and "v2".
+
+    Returns
+    -------
+    A calibrated DuetRun object
     """
     if isinstance(duet_exe_directory, str):
         duet_exe_directory = Path(duet_exe_directory)
@@ -347,36 +88,8 @@ def _run_duet(
         nsp = len(
             np.unique(canopy_grid.values)
         )  # this should be the number of species + 1
-        duet_run = duet.import_duet(duet_exe_directory, nx, ny, nsp, duet_version)
+        duet_run = import_duet(duet_exe_directory, nx, ny, nsp, duet_version)
     return duet_run
-
-
-def _assemble_dataset(duet_run: duet.DuetRun, canopy_grid: DataArray) -> Dataset:
-    """ """
-    litter_density = duet_run.to_numpy("litter", "density")
-    grass_density = duet_run.to_numpy("grass", "density")
-    litter_height = duet_run.to_numpy("litter", "height")
-    grass_height = duet_run.to_numpy("grass", "height")
-
-    # Define fuel parameter and type labels
-    parameter = ["density", "height"]
-    fuel_type = ["grass", "litter"]
-
-    data = np.array(
-        [[grass_density, litter_density], [grass_height, litter_height]]
-    )  # shape: (parameter, fuel_type, x, y)
-
-    # Create an xarray Dataset
-    dataset = Dataset(
-        {"data": (["group", "type", "x", "y"], data)},
-        coords={
-            "parameter": parameter,
-            "fuel_type": fuel_type,
-            "x": canopy_grid["nx"].coords,
-            "y": canopy_grid["ny"].coords,
-        },
-    )
-    return dataset
 
 
 def _execute_duet(
