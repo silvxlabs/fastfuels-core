a2da340c054c39de9565f90fba6b2c9aa9025974
ntutland <56928897+ntutland@users.noreply.github.com>
2025-03-20 08:59:48 -0400

    Save zarr as zip then unzip

--- tests/onramps/create_test_domain.py
+++ tests/onramps/create_test_domain.py
@@ -3,6 +3,7 @@ from os import environ
 environ["FASTFUELS_API_KEY"] = "c51bd50bfde04de7abbfacc15c56c844"
 import geopandas as gpd
 from pathlib import Path
+import zipfile
 from shapely.geometry import Polygon
 from fastfuels_sdk.domains import Domain
 from fastfuels_sdk.features import Features
@@ -39,44 +40,26 @@ domain = Domain.from_geodataframe(
 
 print(f"Created domain with ID: {domain.id}")
 
-# Initialize Features for our domain
-features = Features.from_domain_id(domain.id)
-
-# Create features from OpenStreetMap
-road_feature = features.create_road_feature_from_osm()
-water_feature = features.create_water_feature_from_osm()
-
-# Wait for features to be ready
-road_feature.wait_until_completed()
-water_feature.wait_until_completed()
-
-# Create feature grid
-feature_grid = Grids.from_domain_id(domain.id).create_feature_grid(
-    attributes=["road", "water"],
-)
-
-feature_grid.wait_until_completed()
-
-topography_grid = (
-    TopographyGridBuilder(domain_id=domain.id)
-    .with_elevation_from_3dep(interpolation_method="linear")
-    .build()
-)
-
-topography_grid.wait_until_completed()
-
 # Create tree inventory
 tree_inventory = Inventories.from_domain_id(
     domain.id
-).create_tree_inventory_from_treemap(feature_masks=["road", "water"])
-tree_inventory.wait_until_completed()
+).create_tree_inventory_from_treemap()
+tree_inventory.wait_until_completed(verbose=True)
 
 # Create tree grid
 tree_grid = (
-    TreeGridBuilder(domain_id=domain.id).with_bulk_density_from_tree_inventory().build()
+    TreeGridBuilder(domain_id=domain.id)
+    .with_bulk_density_from_tree_inventory()
+    .with_uniform_fuel_moisture(100)
+    .with_spcd_from_tree_inventory()
+    .build()
 )
-tree_grid.wait_until_completed()
+tree_grid.wait_until_completed(verbose=True)
 
 export = tree_grid.create_export("zarr")
 export.wait_until_completed()
-export.to_file(DUET_DIR / "test_tree_grid.zarr")
+export.to_file(DUET_DIR / "test_tree_grid.zip")
+
+# Exctract tif from compressed download folder and rename
+with zipfile.ZipFile(DUET_DIR / "test_tree_grid.zip") as zf:
+    zf.extractall(path=DUET_DIR / "test_tree_grid.zarr")
